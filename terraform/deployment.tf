ba048eb91ca7de93e91626b9b4558bd3d4ecab9d
baarkerlounger <db@slothlife.xyz>
2021-07-12 18:03:31 +0100

    Add encryption and performance insights

--- terraform/deployment.tf
+++ terraform/deployment.tf
@@ -171,21 +171,23 @@ resource "aws_db_subnet_group" "postgres_subnet_group" {
 }
 
 resource "aws_db_instance" "postgres" {
-  name                    = "DataCollectorPostgres"
-  allocated_storage       = 5 # In gigabytes
-  backup_retention_period = 2
-  backup_window           = "01:00-01:30"
-  maintenance_window      = "sun:03:00-sun:03:30"
-  multi_az                = true
-  engine                  = "postgres"
-  engine_version          = "13.3"
-  storage_encrypted       = false
-  username                = var.database_username
-  password                = var.database_password
-  port                    = 5432
-  instance_class          = "db.t3.micro"
-  db_subnet_group_name    = aws_db_subnet_group.postgres_subnet_group.name
-  vpc_security_group_ids  = [aws_security_group.rds_security_group.id, aws_security_group.ecs_security_group.id]
+  name                         = "DataCollectorPostgres"
+  allocated_storage            = 5 # In gigabytes
+  backup_retention_period      = 2
+  backup_window                = "01:00-01:30"
+  maintenance_window           = "sun:03:00-sun:03:30"
+  multi_az                     = true
+  engine                       = "postgres"
+  engine_version               = "13.3"
+  storage_encrypted            = true
+  username                     = var.database_username
+  password                     = var.database_password
+  port                         = 5432
+  instance_class               = "db.t3.micro"
+  skip_final_snapshot          = true
+  performance_insights_enabled = true
+  db_subnet_group_name         = aws_db_subnet_group.postgres_subnet_group.name
+  vpc_security_group_ids       = [aws_security_group.rds_security_group.id, aws_security_group.ecs_security_group.id]
 
   tags = var.default_tags
 }
