1bcd83239ec6c4fb9a9a9d1b65c44abd17369949
baarkerlounger <db@slothlife.xyz>
2021-07-14 17:44:28 +0100

    Add a test route with an accordion component

--- data-collector/app/javascript/packs/application.js
+++ data-collector/app/javascript/packs/application.js
@@ -2,10 +2,15 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+require.context('govuk-frontend/govuk/assets')
 
+import '../styles/application.scss'
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import { initAll } from 'govuk-frontend'
+
 
 Rails.start()
 ActiveStorage.start()
+initAll()
