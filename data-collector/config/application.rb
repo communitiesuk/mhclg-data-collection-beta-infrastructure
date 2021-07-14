edbcc4c8c4bc8c49e3bb1af7c5794ae3c8611873
baarkerlounger <db@slothlife.xyz>
2021-07-14 15:40:08 +0100

    Pull in fewer dependencies

--- data-collector/config/application.rb
+++ data-collector/config/application.rb
@@ -13,7 +13,7 @@ require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
-require "rails/test_unit/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -31,5 +31,8 @@ module DataCollector
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
   end
 end
