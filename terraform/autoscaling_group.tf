95c0e3d3d9e468bdd32d28f5f4a67c3c3641d685
baarkerlounger <db@slothlife.xyz>
2021-07-15 13:49:11 +0100

    ASG launch config

--- terraform/autoscaling_group.tf
+++ terraform/autoscaling_group.tf
@@ -1,8 +1,10 @@
 resource "aws_launch_configuration" "ecs_launch_config" {
   image_id             = "ami-0d9feb0e9cd3526e4"
   iam_instance_profile = aws_iam_instance_profile.ecs_agent.name
+  name                 = "ecs-cluster"
   security_groups      = [aws_security_group.ecs_security_group.id]
-  user_data            = "#!/bin/bash\necho ECS_Cluster=data-collector-cluster >> /etc/ecs/ecs.config"
+  user_data            = "#!/bin/bash\necho ECS_Cluster=ecs-cluster >> /etc/ecs/ecs.config" # Tell the ECS agent which cluster to join
+
   instance_type        = "t2.micro"
 }
 
