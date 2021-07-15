fcb594a86a08fc4ee082a7121698cc7a2ea47926
baarkerlounger <db@slothlife.xyz>
2021-07-15 10:26:25 +0100

    Output Task definition arn

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,7 @@ output "postgres_endpoint" {
 output "ecr_respository_endpoint" {
   value = aws_ecr_repository.container_repository.repository_url
 }
+
+output "ecs_task_definition" {
+  value = aws_ecs_task_definition.task_definition.arn
+}
