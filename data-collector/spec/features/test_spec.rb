ae0b2e3daaf0138510c5bd55e3286a528a32814f
Matthew Phelan <matthew.phelan@madetech.com>
2021-07-19 16:51:30 +0100

    click link test added

--- data-collector/spec/features/test_spec.rb
+++ data-collector/spec/features/test_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe 'Test Features' do
         visit('/test')
         expect(page).to have_content('Share Lettings and Sales for Social Housing in England Data with MHCLG')
     end
+
+    it 'Links to the About page' do
+        visit('/test')
+        click_link('About')
+        expect(current_path).to eql('/about')
+        expect(page).to have_content('About')
+    end
 end 
