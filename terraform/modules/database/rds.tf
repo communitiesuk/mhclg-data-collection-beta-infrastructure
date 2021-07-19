2080831e67c3ce35a59ce5390a9891d341c0f636
baarkerlounger <db@slothlife.xyz>
2021-07-19 11:19:25 +0100

    Remove db setup stuff from database module

--- terraform/modules/database/rds.tf
+++ terraform/modules/database/rds.tf
@@ -26,28 +26,3 @@ resource "aws_db_instance" "postgres" {
 
   tags = var.default_tags
 }
-
-#TODO actually create the database required so you can go from Terraform to pipeline
-# deploy and running in a single shot. This effectively replaces rake db:create
-provider "postgresql" {
-  host     = aws_db_instance.postgres.endpoint
-  port     = 5432
-  username = var.database_username
-  password = var.database_password
-}
-
-resource "postgresql_role" "application_role" {
-  name               = var.database_username
-  login              = true
-  password           = var.database_password
-  encrypted_password = true
-}
-
-resource "postgresql_database" "data_collector_db" {
-  name              = var.database_name
-  owner             = postgresql_role.application_role.name
-  template          = "template0"
-  lc_collate        = "C"
-  connection_limit  = -1
-  allow_connections = true
-}
